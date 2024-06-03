1a5a8a82c51f1a94eb365f5472eabf03ebe5d97d
Justice-Udehegbunam <justicefavour491@gmail.com>
2024-06-03 21:59:25 +0100

    working on the test api set-up for the project

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/user", userRouter);
 
 mongoose
   .connect(process.env.MONGO_URI)
